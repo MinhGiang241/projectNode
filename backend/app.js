056e99181f5f2361ec342753330ef724090d945d
MinhGiang241 <minhgiang241@gmail.com>
2020-12-18 10:01:35 +0700

    project

---
+++ backend/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const cors = require("cors");
+
+const app = express();
+// app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${new Date().toISOString().replace(/:/g, "-")}-${file.originalname}`
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/png" || "image/jpg" || "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+app.use(multer({ storage, fileFilter }).single("image"));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  const status = err.statusCode || 500;
+  const message = err.message;
+  const data = err.data;
+  res.status(status).json({ message, data });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://minhgiang241:concoc221992@cluster0.wmmld.mongodb.net/messenger"
+  )
+  .then(() => {
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
+  })
+  .catch((err) => console.log(err));
